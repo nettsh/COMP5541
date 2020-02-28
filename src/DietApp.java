b7483846111378fa8ce06ddd5b801dd3e055ef5e
khu0532 <khu0532@gmail.com>
2020-02-28 04:59:41 -0500

    add modify

--- src/DietApp.java
+++ src/DietApp.java
@@ -3,13 +3,16 @@ import java.util.*;
 public class DietApp {
 
     public static void main(String[] args) {
-//        ArrayList<FoodItem> itemsToSave = new ArrayList<FoodItem>();
-//        InDining inDiningItem = new InDining("001", "2020-01-01 1:00AM", "Cake", "200g","Carb");
-//        OutDining outDiningItem = new OutDining("002","2020-02-01 2:00PM","A&W","Frites", "Main Dish");
-//        itemsToSave.add(inDiningItem);
-//        itemsToSave.add(outDiningItem);
         ApplicationController appController = new ApplicationController();
+        //demo read existing items from file and add one indining and one outdining
         appController.retrieveExistingFoodItems("FoodItemsToRead.txt");
+        InDining inDiningItem = new InDining("003", "2020-01-02 1:00AM", "Rice", "200g","Carb");
+        OutDining outDiningItem = new OutDining("004","2020-02-02 2:00PM","Second Cup","Cookies", "Main Dish");
+        appController.add(inDiningItem);
+        appController.add(outDiningItem);
+        //Modify uses Id to match
+        inDiningItem.setServing("1kg");
+        appController.modify(inDiningItem);
         appController.saveFoodItems("FoodItemsSaved.txt");
     }
 }
